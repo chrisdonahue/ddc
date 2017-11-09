bff9b7d39452c250fba2426828902051b2067036
Chris Donahue <christopherdonahue@gmail.com>
2017-11-09 02:08:26 -0800

    simplified tf_Feats

--- ddc/tf_feats.py
+++ ddc/tf_feats.py
@@ -1,4 +1,20 @@
+import os
+
 import tensorflow as tf
+
+
+# input shape: [] (placeholder tf.string)
+# output shape: [?, nch] (samples)
+def load_audio_file(audio_fp, fs=44100, nch=2):
+  audio_bin = tf.read_file(audio_fp)
+  file_format = os.path.splitext(audio_fp)[1][1:]
+  samples = tf.contrib.ffmpeg(
+      song_binary,
+      file_format=file_format,
+      samples_per_second=fs,
+      channel_count=nch)
+  return samples
+
 
 # input shape:  [None] (raw waveform)
 # output shape: [None, nfeats]
